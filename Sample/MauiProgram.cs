b316b330713f9d605a4176755197d6d736b8158e
Alex Dobrynin <shuron1988@gmail.com>
2025-05-12 17:39:28 +0200

    added .net 9 support

--- Sample/MauiProgram.cs
+++ Sample/MauiProgram.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.Extensions.Logging;
-
-namespace Sample
+﻿namespace Sample
 {
     public static class MauiProgram
     {
@@ -14,10 +12,6 @@ namespace Sample
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
-
-#if DEBUG
-    		builder.Logging.AddDebug();
-#endif
 
             return builder.Build();
         }
